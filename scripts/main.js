ae014f08b19f3f227d87f0435d23a6895795fa7b
CantarinoG <guilhermecantarino1709@gmail.com>
2022-10-29 19:14:49 -0300

    All the functions are working properly.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 let slider = document.querySelector('#slider');
 let sliderValue = slider.value;
 
+let colorPicker = document.querySelector('#color-picker');
+
 let gridInfo = document.querySelector('#grid-info');
 
 let board = document.querySelector('.board');
@@ -11,6 +13,53 @@ let btnShade = document.querySelector('#shading');
 let btnRainbow = document.querySelector('#rainbow');
 let btnClear = document.querySelector('#clear');
 
+let mouseDown;
+
+let squares;
+
+document.addEventListener('mousedown', function() {
+    mouseDown = true;
+});
+
+document.addEventListener('mouseup', function() {
+    mouseDown = false;
+});
+
+
+function addSquaresListeners() {
+    squares.forEach(square => {
+        square.addEventListener('mouseenter', () => {
+            if (mouseDown) {
+                if (btnDraw.classList.contains('btn-active')) {
+                    square.style.backgroundColor = colorPicker.value;
+                } else if (btnErase.classList.contains('btn-active')) {
+                    square.style.backgroundColor = 'rgb(255, 255, 255)';
+                } else if (btnShade.classList.contains('btn-active')) {
+                    let brightness = square.style.filter;
+                    let brightnessValue = +brightness.slice(11, 14) - 0.1;
+                    square.style.filter = `brightness(${brightnessValue})`;
+                } else if (btnRainbow.classList.contains('btn-active')) {
+                    square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+                }
+            }
+        });
+
+        square.addEventListener('click', () => {
+            if (btnDraw.classList.contains('btn-active')) {
+                square.style.backgroundColor = colorPicker.value;
+            } else if (btnErase.classList.contains('btn-active')) {
+                square.style.backgroundColor = 'rgb(255, 255, 255)';
+            } else if (btnShade.classList.contains('btn-active')) {
+                let brightness = square.style.filter;
+                let brightnessValue = +brightness.slice(11, 14) - 0.1;
+                square.style.filter = `brightness(${brightnessValue})`;
+            } else if (btnRainbow.classList.contains('btn-active')) {
+                square.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+            }
+        });
+
+    });
+}
 
 function createBoard() {
     let boardSize = board.offsetWidth;
@@ -24,6 +73,9 @@ function createBoard() {
         boardHtml += "</div>";
     }
     board.innerHTML = boardHtml;
+    squares = document.querySelectorAll('.column');
+    addSquaresListeners();
+    clearBoard();
 }
 
 slider.addEventListener('input', () => {
@@ -59,8 +111,16 @@ btnRainbow.addEventListener('click', () => {
     btnRainbow.classList.add('btn-active');
 })
 
-btnClear.addEventListener('click', () => {
+btnClear.addEventListener('click', clearBoard);
 
-})
+function clearBoard() {
+    squares.forEach(square => {
+        square.style.backgroundColor = 'rgb(255, 255, 255)';
+        square.style.filter = 'brightness(1.001)';
+        /*It needs to be 1.001 instead of 1.0, because when we get the filter it would return 'brightness(1)', 
+        and we need at least three chars to slice the value out of the string so we can update the brightness with
+        the shading mode. Not the best solution, I know...*/
+    })
+}
 
 createBoard();
